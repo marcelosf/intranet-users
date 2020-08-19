93c288e778f34e6b350f90a5c3fd410267541c67
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-19 10:19:17 -0300

    Adiciona model para usuário
    Adiciona um model customizado para User.

--- users/accounts/models.py
+++ users/accounts/models.py
@@ -1,9 +1,14 @@
+import json
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
+from django.core.mail import send_mail
+from django.conf import settings
+
+from users.accounts.managers import UserManager
 
 
-class UserModel(AbstractBaseUser):
+class UserModel(AbstractBaseUser, PermissionsMixin):
     login = models.CharField(_("login"), max_length=50, unique=True)
     name = models.CharField(_("name"), max_length=100)
     user_type = models.CharField(_("user type"), max_length=1)
@@ -19,11 +24,46 @@ class UserModel(AbstractBaseUser):
     date_joined = models.DateTimeField(_("Joined at"), auto_now_add=True)
 
     USERNAME_FIELD = 'login'
+    REQUIRED_FIELDS = ['name', 'type', 'main_email']
     EMAIL_FIELD = 'main_email'
+    ALLOWED_UNIDADES = settings.ALLOWED_UNIDADES
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    objects = UserManager()
 
     def get_full_name(self):
         return self.name
 
     def get_short_name(self):
-        short_name = self.name.split()[0]
-        return short_name
+        names = self.name.split()
+        return names[0]
+
+    def email_user(self, subject, message, from_email=None):
+        send_mail(subject, message, from_email, [self.main_email])
+
+    def get_phone(self):
+        return self.formatted_phone
+
+    def is_servidor(self):
+        bind = self.get_bind()
+        for item in bind:
+            if item["tipoVinculo"] == 'SERVIDOR':
+                return True
+
+        return False
+
+    def unidade_is_allowed(self):
+        bind = self.get_bind()
+        for item in bind:
+            codigo_unidade = item["codigoUnidade"]
+            if codigo_unidade in self.ALLOWED_UNIDADES:
+                return True
+
+        return False
+
+    def get_bind(self):
+        bind = self.bind.replace("'", '"')
+        return json.loads(bind)
--- users/accounts/tests/test_accounts_models.py
+++ users/accounts/tests/test_accounts_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core import mail
 from django.contrib.auth.models import AbstractBaseUser
 
 from users.accounts.models import UserModel
@@ -26,3 +27,52 @@ class UserModelTest(TestCase):
 
     def test_has_attribute_get_short_name(self):
         self.assertTrue(hasattr(self.obj, 'get_short_name'))
+
+    def test_create_user(self):
+        """User must exists on database"""
+        self.make_user()
+        self.assertTrue(UserModel.objects.exists())
+
+    def test_get_full_name(self):
+        user = self.make_user()
+        self.assertEqual('Marc Stold Further', user.get_full_name())
+
+    def test_get_short_name(self):
+        user = self.make_user()
+        self.assertEqual('Marc', user.get_short_name())
+
+    def test_email_user(self):
+        user = self.make_user()
+        user.email_user(subject='email test', message='message test',
+                        from_email='acesso@test.com')
+        self.assertTrue(mail.outbox[0])
+
+    def test_get_phone(self):
+        user = self.make_user(formatted_phone='3334455')
+        self.assertEqual('3334455', user.get_phone())
+
+    def test_is_servidor(self):
+        bind = "[{'tipoVinculo': 'ALUNO'}, {'tipoVinculo': 'SERVIDOR'}]"
+        user = self.make_user(bind=bind)
+        self.assertTrue(user.is_servidor())
+
+    def test_is_servidor_not(self):
+        bind = "[{'tipoVinculo': 'ALUNO'}, {'tipoVinculo': 'ALUNO'}]"
+        user = self.make_user(bind=bind)
+        self.assertFalse(user.is_servidor())
+
+    def test_unidade_is_allowed(self):
+        bind = "[{'codigoUnidade': '12'}, {'codigoUnidade': '14', 'tipoVinculo': 'SERVIDOR'}]"
+        user = self.make_user(bind=bind)
+        self.assertTrue(user.unidade_is_allowed())
+
+    def test_unidade_is_allowed_not(self):
+        bind = "[{'codigoUnidade': '12'}, {'codigoUnidade': '13', 'tipoVinculo': 'SERVIDOR'}]"
+        user = self.make_user(bind=bind)
+        self.assertFalse(user.unidade_is_allowed())
+
+    def make_user(self, **kwargs):
+        default = dict(login='4444444', main_email='main@test.com', password='92874',
+                       name='Marc Stold Further', type='I ')
+        data = dict(default, **kwargs)
+        return UserModel.objects.create_user(**data)
